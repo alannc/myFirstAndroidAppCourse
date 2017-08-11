0669587e79edacd2e7ff38505096a9ece48a2b41
Alannc <alannnc@gmail.com>
2017-08-10 20:36:04 -0500

    Increased hello world size, and bolded it

--- app/src/main/java/com/alannnc/myfirstapp/alanmyfirstapp/MainActivity.java
+++ app/src/main/java/com/alannnc/myfirstapp/alanmyfirstapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.alannnc.myfirstapp.alanmyfirstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickButton(View view) {
+        Log.v("Demo", "Button clicked");
     }
 }
